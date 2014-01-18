993c5cb988d5e601072e0dcc653b6808ace6418e
Yin <yinxiaoyu888@gmail.com>
2014-01-18 21:09:02 +0800

    Initial Commit

---
+++ HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Yin on 14-1-18.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by Yin on 14-1-18.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
